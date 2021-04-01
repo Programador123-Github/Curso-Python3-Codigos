d320ca7cf034cdeb7d8dcd6cf3fb32ead5807237
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-01 19:46:12 -0300

    Add files via upload

---
+++ (Codigo)_Unidad1_Leccion1.py
@@ -0,0 +1,20 @@
+# Este programa es un ejemplo para el uso de los numeros y variables.
+# Numero Entero		: 1
+# Numero Decimal	: 1.0	(Numeros Flotantes)
+# Operadores Aritemeticos:
+	# +
+	# -
+	# /
+	# //		(Division sin numero decimal)
+	# *
+	# **
+	# %
+
+# Reglas de las variables:
+	# Sin espacios.
+	# Sin simbolos raros excepto "_".
+	# No puede empezar por numero.
+
+# Asignacion:
+	# Crear un programa que imprima por pantalla la multiplicacion de dos variables.
+
