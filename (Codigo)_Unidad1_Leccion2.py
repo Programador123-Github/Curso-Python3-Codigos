ed52f629c53b3ebd39b602de463213b50131b374
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-02 11:55:45 -0300

    Add files via upload

---
+++ (Codigo)_Unidad1_Leccion2.py
@@ -0,0 +1,32 @@
+# Tipos de formas para escribir cadenas:
+	# "Ejemplo"
+	# 'Ejemplo'
+	# "'Ejemplo'"
+	# '"Ejemplo"'
+	# """ "Ejemplo" """
+	# ''' 'Ejemplo' '''
+
+# Caracteres Especiales en las Cadenas:
+	# \n 	: New Line
+	# \t 	: TAB
+
+# Prefijos de las cadenas:
+	# r''	: Raw
+	# f''   : Format
+
+# Ejemplo de raw r'':
+print(r"C:\nombre\\carpeta") # Intenta ahora sin el prefijo, para que veas el resultado.
+
+
+# Ejemplo de format f'':
+nombre = "Andre"
+print(f"Hola {nombre}")
+
+# Ejemplo de new line:
+print("Primera linea\nSegunda Linea.")
+
+# Ejemplo de tab:
+print("Hola\tMundo")
+
+
+
