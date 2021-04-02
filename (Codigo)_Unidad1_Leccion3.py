81cb91b67fe6205400ff61a49d4bc33b23784cac
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-02 19:09:00 -0300

    Add files via upload

---
+++ (Codigo)_Unidad1_Leccion3.py
@@ -0,0 +1,17 @@
+# Indexing (Accedemos al valor de una determinada posicion):
+cadena = "Ejemplo"
+print(cadena[0])
+
+# Slicing (comenzamos de X posicion hasta el final):
+cadena2 = "Ejemplo 2"
+print(cadena2[3:len(cadena2)+1])
+
+# Slicing con numeros negativos:
+cadena3 = 'Ejemplo 3'
+print(cadena3[:-4])
+
+# Invertimos la cadena:
+cadena4 = 'Ejemplo 4'
+print(cadena4[::-1])
+
+
