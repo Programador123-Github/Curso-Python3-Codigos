06b2e57ca984409978dc2a43c07bdd1b25b75511
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-04 12:40:56 -0300

    Add files via upload

---
+++ (Codigo)_Unidad1_Leccion5.py
@@ -0,0 +1,8 @@
+
+# input() recibe un valor por teclado y lo guarda en la variable.
+# int() tranforma la cadena a numero entero. Si no es un numero o contiene caracteres ocurrira un error en convertirlo.
+
+numero1 = int(input("Escribe un numero entero para sumar: "))
+numero2 = int(input("Escribe otro numero entero para sumar: "))
+
+print(numero1+numero2)
