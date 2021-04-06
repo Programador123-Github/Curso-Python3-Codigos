415afe00330711f5ac575b4458535df1e360fab1
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-06 18:42:37 -0300

    Add files via upload

---
+++ (Codigo)_Unidad2_Leccion5.py
@@ -0,0 +1,8 @@
+
+# not 	: Negacion
+# and	: Ambos deben ser verdad o retorna False
+# or	: Alguno de ambos deben ser verdad sino entonces es False.
+
+print(not 'b' != 'b')
+print('a' != 'a' and 'b' != 'b')
+print('a' != 'a' or 'b' != 'b')
