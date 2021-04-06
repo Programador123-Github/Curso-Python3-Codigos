5ed4a1d4163acd455dc542dd6730e6d0c24b59b4
Programador123-Github <77536149+Programador123-Github@users.noreply.github.com>
2021-04-06 18:41:42 -0300

    Add files via upload

---
+++ (Codigo)_Unidad2_Leccion4.py
@@ -0,0 +1,12 @@
+# == 	: Igualdad
+# != 	: Distinto de
+# >=	: Mayor o igual que
+# <=	: Menor o igual que
+# <		: Menor que
+# > 	: Mayor que
+
+numero1 = 3
+numero2 = 2
+
+respuesta = numero1 > numero2
+print(type(respuesta))
